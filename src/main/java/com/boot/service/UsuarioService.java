d3b8bf6f4b6ead1830f8cae821dba281b5d69e01
mgirlene <mgirlene.silva0908@gmail.com>
2020-12-21 14:41:13 -0300

    Testes

--- src/main/java/com/boot/service/UsuarioService.java
+++ src/main/java/com/boot/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.boot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -19,14 +20,15 @@ public class UsuarioService {
 				.uri("api/usuario/save")
 				.retrieve()
 				.bodyToMono(Usuario.class);
-		
-		Usuario user = mono.block();
-		
-		return user;
+
+		Mono<Usuario> user = Mono.just(mono).block();
+				
+		return user.block();
 	}
 	
 	public Usuario findEmailSenha(Usuario usuario) {
-		Mono<Usuario> mono = this.webClient.post()
+		Mono<Usuario> mono = this.webClient
+				.method(HttpMethod.POST)
 				.uri("api/usuario/findemailsenha")
 				.retrieve()
 				.bodyToMono(Usuario.class);
